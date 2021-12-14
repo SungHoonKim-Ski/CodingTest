42d3753671e579049463f074ebb5925afffe48f9
Ski-K <diwlgns2@naver.com>
2021-12-15 06:40:49 +0900

    Revert "Improve 1110"
    This reverts commit c8e674354c0bfed23dc7fd044c11a27bdc69cd81.

--- Baekjoon/1110.java
+++ Baekjoon/1110.java
@@ -4,23 +4,24 @@ public class Main {
 
     public static void main(String args[]) throws Exception{ // Q.1110
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputNum = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int inputNum = sc.nextInt();
         if(inputNum > 99){
             return;
         }
 
         int cycleCount = 0;
         int curNum = inputNum;
-        int num1;
-        int num10;
         do{
-            cycleCount++;
+            ++cycleCount;
 
-            num1 = curNum % 10;
-            num10 = curNum / 10;
-            curNum = (10 * num1) + (num1 + num10) % 10;
-
+            if(curNum < 10){
+                curNum *= 11;
+            }else{
+                int num1 = curNum % 10;
+                int num10 = curNum / 10;
+                curNum = (10 * num1) + (num1 + num10) % 10;
+            }
         }while (curNum != inputNum);
         System.out.println(cycleCount);
         return;
