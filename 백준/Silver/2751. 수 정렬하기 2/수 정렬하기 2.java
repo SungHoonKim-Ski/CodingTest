8b07c34e0eb7ff41406b8c07d7a01cfba099120e
SeoungHoon-Kim <57925467+SeoungHoon-Kim@users.noreply.github.com>
2022-09-04 16:59:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1780 ms, Memory: 142948 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    //    private static Scanner sc = new Scanner(System.in);
+    private static BufferedReader br;
+    private static StringBuffer sb;
+
+    public static void main(String args[]) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuffer();
+
+        int count = Integer.parseInt(br.readLine());
+        int[] sortArr = new int[count];
+        for(int i=0; i<count; i++){
+            sortArr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(sortArr);
+        for(int i=0; i<count; i++){
+            sb.append(sortArr[i] + " ");
+        }
+        System.out.println(sb);
+    }
+    
+}
