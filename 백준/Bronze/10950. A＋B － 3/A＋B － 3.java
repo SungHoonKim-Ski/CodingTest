14ff860c5584861ea7d7eba1adc87a323ba972d0
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-01-12 16:42:18 +0900

    [Bronze V] Title: A+B - 3, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a + b).append('\n');
+        }
+        sb.setLength(sb.length()-1);
+        System.out.println(sb);
+    }
+}
