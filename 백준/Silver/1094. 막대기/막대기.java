ea6b60ded8cf3c8962304596a118ae4f71d1e2c3
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-01-20 23:40:30 +0900

    [Silver V] Title: 막대기, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Main
 {
     public static void main(String args[]) throws Exception
     {
 
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        char[] binaryInput = Integer.toBinaryString(T).toCharArray();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] binaryInput = Integer.toBinaryString(Integer.parseInt(br.readLine())).toCharArray();
         int res = 0;
         for (int i=0; i< binaryInput.length;i++) {
             if (binaryInput[i] == '1') res++;
