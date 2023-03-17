eb6a871decf41f62e4ff07efa10a1aa24c797f10
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-03-17 14:11:34 +0900

    [Silver IV] Title: 듣보잡, Time: 312 ms, Memory: 23420 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,51 +1,115 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-
 public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-    //    private static Scanner sc = new Scanner(System.in);
+    static int N, M;
+    static String[] A;
 
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-
-        String[] inputSplit = br.readLine().split(" ");
-
-        int cantListenCount = Integer.parseInt(inputSplit[0]);
-        int cantSeeCount = Integer.parseInt(inputSplit[1]);
-
-        HashSet<String> cantListenSet = new HashSet<>();
-        List<String> cantListenSeeList = new ArrayList<>();
-
-        for(int i=0; i<cantListenCount; i++){
-            cantListenSet.add(br.readLine());
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        A = new String[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = scan.nextLine();
         }
-
-        int cantListenSeeCount = 0;
-        for(int i=0; i<cantSeeCount; i++){
-            String cantSee = br.readLine();
-            if(cantListenSet.contains(cantSee)){
-                cantListenSeeList.add(cantSee);
-                cantListenSeeCount++;
-            }
-        }
-        Collections.sort(cantListenSeeList);
-
-        sb.append(cantListenSeeCount);
-        sb.append("\n");
-        for(String cantListenSee : cantListenSeeList){
-            sb.append(cantListenSee);
-            sb.append("\n");
-        }
-
-        sb.setLength(sb.length()-1);
-        System.out.println(sb);
-        br.close();
-        sb.setLength(0);
     }
 
+    static boolean bin_search(int[] A, int L, int R, int X) {
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (A[mid] == X) return true;
+            if (A[mid] <= X)
+                L = mid + 1;
+            else{
+                R = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    static boolean bin_search(String[] A, int L,int R, String X){
+        while (L <= R){
+            int mid = (L + R) / 2;
+            if (A[mid].equals(X))
+                return true;
+
+            if (A[mid].compareTo(X) < 0)
+                L = mid + 1;
+            else
+                R = mid - 1;
+        }
+        return false;
+    }
+    static void pro() {
+
+        Arrays.sort(A, 1, N + 1);
+        int res = 0;
+        String[] resStr = new String[M];
+        for (int i = 1; i <= M; i++) {
+            String input = scan.nextLine();
+            if(bin_search(A, 1, N, input)) {
+                resStr[++res] = input;
+            }
+        }
+        Arrays.sort(resStr, 1, res + 1);
+        System.out.println(res);
+        for (int i = 1; i <= res; i++) sb.append(resStr[i]).append('\n');
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
