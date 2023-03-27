0e496cdaae40dd8f2892f7f5497a34342aedf735
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-03-27 16:04:42 +0900

    [Unrated] Title: 연결 요소의 개수, Time: 540 ms, Memory: 114236 KB -BaekjoonHub

--- 백준/Unrated/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Unrated/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -34,53 +34,34 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             if (graph[start][i] && !visit[i]) {
                 dfs(i);
+//                bfs(i);
             }
         }
     }
-//    static void pro() {
-//
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < N; j++) {
-//                cur = 0;
-//                dfs(i, j);
-//                if (cur != 0) houseCnt.add(cur);
-//            }
-//        }
-//
-//        Collections.sort(houseCnt);
-//        sb.append(cnt).append('\n');
-//        for (int i: houseCnt) {
-//            sb.append(i).append('\n');
-//        }
-//        System.out.println(sb);
-//    }
 
-//    static void bfs(int y, int x) {
-//
-//        Queue<int[]> que = new LinkedList<>();
-//        arr[y][x] = false;
-//        que.add(new int[]{y, x});
-//
-//        while (!que.isEmpty()) {
-//            int[] point = que.poll();
-//            for (int i = 0; i < 4; i++) {
-//                int nextY = point[0] + dy[i];
-//                int nextX = point[1] + dx[i];
-//                if (nextX == -1 || nextX == arrX || nextY == -1 || nextY == arrY) continue;
-//                if (arr[nextY][nextX]) {
-//                    que.add(new int[]{nextY, nextX});
-//                    arr[nextY][nextX] = false;
-//                }
-//            }
-//        }
-//    }
+    static void bfs(int start) {
+
+        Queue<Integer> que = new LinkedList<>();
+        visit[start] = true;
+        que.add(start);
+
+        while (!que.isEmpty()) {
+            int s = que.poll();
+            for (int i = 1; i <= N; i++) {
+                if (graph[s][i] && !visit[i]) {
+                    que.add(i);
+                    visit[i] = true;
+                }
+            }
+        }
+    }
 
     static void pro() {
 
         int cnt = 0;
         for (int i = 1; i <= N;  i++) {
             if (!visit[i]) {
-                dfs(i);
+                bfs(i);
                 cnt++;
             }
         }
