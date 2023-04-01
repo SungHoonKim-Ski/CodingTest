fa2acbcfdea82e7ba99cc069b8ed731c91d0c5d8
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-04-01 16:09:58 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,50 +1,120 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-class Main {
-    static BufferedReader br;
-    static StringBuffer sb;
 
-    public static void main(String[] args) throws IOException {
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuffer();
+    static int V, E;
+    static List<Integer>[] list;
+    static boolean[] visit;
 
-        int n = Integer.parseInt(br.readLine()) + 1;
-        int m = Integer.parseInt(br.readLine());
+    static int res = 0;
 
-        int[][] arr = new int[n][n];
-        StringTokenizer st;
-        for (int i=0; i<m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+    static void input() {
 
-            arr[x][y] = 1;
-            arr[y][x] = 1;
+        V = scan.nextInt();
+        E = scan.nextInt();
+        visit = new boolean[V + 1];
+        list = new List[V + 1];
+        for (int i = 1; i <= V; i++) list[i] = new ArrayList<>();
+        for (int i = 0; i < E; i++) {
+            int s = scan.nextInt();
+            int e = scan.nextInt();
+            list[s].add(e);
+            list[e].add(s);
+        }
+    }
+
+    static int bfs() {
+
+        int cnt = 0;
+        Queue<Integer> que = new LinkedList<>();
+        visit[1] = true;
+        for (int v: list[1]) {
+            que.add(v);
+            visit[v] = true;
         }
 
-        boolean[] isWarm = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        while (!queue.isEmpty()) {
-            int warm = queue.poll();
-            isWarm[warm] = true;
-
-            for (int i=1; i < n; i++) {
-                if (!isWarm[i] && arr[warm][i] == 1) {
-                    queue.add(i);
+        while (!que.isEmpty()) {
+            int v =  que.poll();
+            cnt++;
+            for (int i: list[v]) {
+                if (!visit[i]) {
+                    que.add(i);
+                    visit[i] = true;
                 }
             }
         }
+        return cnt;
+    }
 
-        int res = 0;
-        for (int i=2; i<n; i++) {
-            if (isWarm[i]) res++;
+    static void dfs(int v) {
+        visit[v] = true;
+        for (int i: list[v]) {
+            if (!visit[i]) {
+                res++;
+                dfs(i);
+            }
+        }
+    }
+
+    static void pro() {
+//        System.out.println(bfs());
+        dfs(1);
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        System.out.println(res);
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
