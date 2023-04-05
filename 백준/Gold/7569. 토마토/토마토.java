ed8bfa10a247d1bfeeab8065de78c078e83d0a0e
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-04-05 15:46:06 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 129036 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,81 +1,129 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 
 public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-    //    private static Scanner sc = new Scanner(System.in);
-    private static BufferedReader br;
-    private static StringBuffer sb;
+    static int M, N, H, cnt = 0;
+    static int[][][] graph;
+    static boolean[][][] visit;
 
-    public static void main(String args[]) throws IOException {
+    static int[] dx = {1, 0, -1, 0, 0, 0};
+    static int[] dy = {0, 1, 0, -1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, 1, -1};
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuffer();
+    static Queue<int[]> queue;
+    static void input() {
 
-        String[] input = br.readLine().split(" ");
-        int x = Integer.parseInt(input[0]);
-        int y = Integer.parseInt(input[1]);
-        int z = Integer.parseInt(input[2]);
+        M = scan.nextInt();
+        N = scan.nextInt();
+        H = scan.nextInt();
 
-        int zeroCount = 0;
-        int[][][] tomato = new int[z][y][x];
-        Queue<int[]> queue = new LinkedList<>();
-        for(int i=0; i<z; i++){
-            for(int j=0; j<y; j++){
-                input = br.readLine().split(" ");
-                for(int k=0; k<x; k++){
-                    int tmp = Integer.parseInt(input[k]);
-                    if(tmp == 0){
-                        zeroCount++;
-                    }else if(tmp == 1){
-                        queue.add(new int[]{i, j, k});
-                    }
-                    tomato[i][j][k] = tmp;
-                }
-            }
-        }
-
-        int dayCount = 0;
-        int[] dx = {1, -1, 0, 0, 0, 0};
-        int[] dy = {0, 0, 1, -1, 0, 0};
-        int[] dz = {0, 0, 0, 0, 1, -1};
-
-        while (zeroCount > 0){
-            Queue<int[]> nextDayQueue = new LinkedList<>();
-            boolean isChanged = false;
-            while (!queue.isEmpty()){
-                int[] freshTomato = queue.poll();
-                int curZ = freshTomato[0];
-                int curY = freshTomato[1];
-                int curX = freshTomato[2];
-
-                for(int i=0; i<6; i++){
-                    int tmpZ = curZ + dz[i];
-                    int tmpY = curY + dy[i];
-                    int tmpX = curX + dx[i];
-
-                    if(tmpZ < 0 || tmpZ == z || tmpX < 0 || tmpY < 0 || tmpX == x || tmpY == y) continue;
-
-                    if(tomato[tmpZ][tmpY][tmpX] == 0){
-                        tomato[tmpZ][tmpY][tmpX]++;
-                        zeroCount--;
-                        isChanged = true;
-                        nextDayQueue.add(new int[]{tmpZ, tmpY, tmpX});
+        graph = new int[H][N][M];
+        visit = new boolean[H][N][M];
+        queue = new LinkedList<>();
+        
+        for (int h = 0; h < H; h++) {
+            for (int n = 0; n < N; n++) {
+                for (int m = 0; m < M; m++) {
+                    int input = scan.nextInt();
+                    if (input != 0) {
+                        graph[h][n][m] = input;
+                        visit[h][n][m] = true;
+                        if (input == 1) {
+                            queue.add(new int[] {h, n, m, 0});
+                        }
+                    }else {
+                        cnt++;
                     }
                 }
             }
-            if(!isChanged){
-                dayCount = -1;
-                break;
-            }
-            queue.addAll(nextDayQueue);
-            dayCount++;
         }
-
-        System.out.println(dayCount);
     }
 
+    static int bfs() {
+
+        int day = 0;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll(); // z, y, x, day
+            for (int i = 0; i < 6; i++) {
+
+                int nz = cur[0] + dz[i];
+                int ny = cur[1] + dy[i];
+                int nx = cur[2] + dx[i];
+                if (nz == H || ny == N || nx == M || nz < 0 || ny < 0 || nx < 0) continue;
+                if (visit[nz][ny][nx]) continue;
+                visit[nz][ny][nx] = true;
+                queue.add(new int[] {nz, ny, nx, cur[3] + 1});
+                day = cur[3] + 1;
+                cnt--;
+            }
+        }
+        if (cnt > 0) return -1;
+        else return day;
+    }
+
+    static void pro() {
+
+        System.out.println(bfs());
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        // BufferedReader의 빠른 속도,
+        // + Scanner의 타입별 입력값을 받는 기능을 혼합
+        // (자바 내부에 구현된 Scanner는 속도가 느림)
+        // 다른분의 코드를 참고한 코드
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
