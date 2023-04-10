71d1eec280737e99f64efdbb474518855f203bcd
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-04-10 17:45:37 +0900

    [Unrated] Title: A-B, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Unrated/1001. A－B/A－B.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static void input() {
+
+        n = scan.nextInt();
+        m = scan.nextInt();
+    }
+
+    // dfs(x, par) := 정점 x 의 부모가 par 였고, x의 children들을 찾아주는 함수
+//    static void dfs(int x, int par) {
+//
+//        for (int y: adj[x]) {
+//            if (y == par) continue;
+//            parent[y] = x;
+//            dfs(y, x);
+//        }
+//    }
+
+    static void pro() {
+        // 1 번 정점이 ROOT 이므로, 여기서 시작해서 Tree의 구조를 파악하자.
+        sb.append(n - m);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+        System.out.println(sb);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
