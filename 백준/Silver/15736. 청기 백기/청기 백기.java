42d9f0f2ca40e26e495f7454f0268763a38b68ba
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-04-25 15:49:55 +0900

    [Silver IV] Title: 청기 백기, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    static int n;
+    public static void main(String[] args) throws IOException {
+
+        input();
+        pro();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+    }
+
+    static void pro() {
+
+        int cnt = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+}
