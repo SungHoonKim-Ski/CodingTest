7a6addae03bd0b99632219a3dfb54b2b0c0fa7ab
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-04-25 22:24:58 +0900

    [Gold III] Title: 줄 세우기, Time: 500 ms, Memory: 46004 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] indeg;
+    static ArrayList<Integer>[] adj;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        adj = new ArrayList[N + 1];
+        indeg = new int[N + 1];
+        for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
+        while (M-- > 0) {
+            int s = scan.nextInt();
+            int e = scan.nextInt();
+            adj[s].add(e);
+            indeg[e]++;
+        }
+    }
+
+    static void pro() {
+        Deque<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) if (indeg[i] == 0) queue.add(i);
+        while (!queue.isEmpty()) {
+
+            int cur = queue.poll();
+            sb.append(cur).append(' ');
+            for (int v: adj[cur]) {
+                indeg[v]--;
+                if (indeg[v] == 0) queue.add(v);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
