647ef25b73b92ac267059b74aeb714988baf9251
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-05-09 22:37:38 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 368 ms, Memory: 68408 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static char[][] starsArr;
+    public static void main(String args[])throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        starsArr = new char[n][n];
+        calStars(0,0,n,false);
+
+
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                sb.append(starsArr[i][j]);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        return;
+    }
+
+    private static void calStars(int x, int y, int n, boolean isBlank){
+        if(isBlank){
+            for(int i=x; i<x+n; i++){
+                for(int j=y; j<y+n; j++){
+                    starsArr[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        if(n == 1){
+            starsArr[x][y] = '*';
+            return;
+        }
+
+
+        int size = n/3;
+        int count = 0;
+        for(int i=x; i<x+n; i+=size){
+            for(int j=y; j<y+n; j+=size){
+                calStars(i, j, size, (++count == 5) ? true : false);
+            }
+        }
+    }
+}
