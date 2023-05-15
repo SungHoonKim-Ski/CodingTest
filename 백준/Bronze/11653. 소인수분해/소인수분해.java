4e9130ae0d8f0b75622588421c665ab3d1f51448
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-05-15 23:59:03 +0900

    [Bronze I] Title: 소인수분해, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        
+		int N = in.nextInt();
+ 
+		for (int i = 2; i <= Math.sqrt(N); i++) {	// 또는 i * i <= N
+			while (N % i == 0) {
+				System.out.println(i);
+				N /= i;
+			}
+		}
+		if (N != 1) {
+			System.out.println(N);
+		}
+	}
+}
