254cfc35e5e3ad2b8f82793d4421f6cd2685f074
SungHoon Kim <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-06-21 23:45:08 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+
+//    static int N;
+
+    public static void input() throws Exception
+    {
+//        N = Integer.parseInt(br.readLine());
+    }
+
+    static void pro()
+    {
+//        boolean isEven = true;
+//        if (N % 2 != 0) isEven = false;
+//
+//        long[] dp = new long[N + 1];
+//
+//        dp[0] = 0;
+//        dp[1] = dp[2] = 1;
+//
+//        for (int i = 3; i <= N; i++) {
+//            dp[i] = dp[i - 1] + dp[i - 2];
+//        }
+//        System.out.println(dp[N]);
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        input();
+        pro();
+    }
+}
