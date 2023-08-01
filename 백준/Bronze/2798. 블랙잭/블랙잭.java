19d42e1ca0ee0e3a3f83343d5be47f6b173e925f
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-08-01 11:35:50 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,42 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
 
 public class Main {
 
-    //    private static Scanner sc = new Scanner(System.in);
-    private static BufferedReader br;
-    private static StringBuffer sb;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String args[]) throws IOException {
+    static StringTokenizer st;
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuffer();
+    static int N, M, Max = -1;
+    static int[] InputArr;
 
-        String[] inputArr = br.readLine().split(" ");
-        int n = Integer.parseInt(inputArr[0]);
-        int m = Integer.parseInt(inputArr[1]);
+    static boolean[] Visit;
 
-        int[] cards = new int[n];
-        inputArr = br.readLine().split(" ");
-        for(int i=0; i<n; i++){
-            cards[i] = Integer.parseInt(inputArr[i]);
-        }
 
-        int max = 0;
-        for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
-                for(int k=j+1; k<n; k++){
-                    int sum = cards[i] + cards[j] + cards[k];
-                    if(max < sum && sum <= m){
-                        max = sum;
-                    }
-                }
-            }
-        }
-        System.out.println(max);
+    static void input() throws IOException
+    {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        InputArr = new int[N];
+        for (int i = 0; i < N; i++) InputArr[i] = Integer.parseInt(st.nextToken());
+
+        Visit = new boolean[N];
+    }
+
+    static void pro() throws IOException
+    {
+    	
+    	for (int i = 0; i < N - 2; i++) {
+    		for (int j = i + 1; j < N - 1; j++) {
+    			for (int k = j + 1; k < N; k++) {
+    				int sum = InputArr[i] + InputArr[j] + InputArr[k];
+    				if (sum <= M) Max = Math.max(Max, sum);
+    			}
+    		}
+    	}
+    	
+        System.out.println(Max);
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        input();
+        pro();
+
     }
 
 }
