6840e570ecd5f9ed876dd9127d81ed280b761092
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-08-04 23:00:15 +0900

    [Gold IV] Title: 뱀, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -32,7 +32,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int appleY = Integer.parseInt(st.nextToken()) - 1;
             int appleX = Integer.parseInt(st.nextToken()) - 1;
-            board[appleY][appleX] = 1;
+            board[appleY][appleX] = 1; // 사과 위치 = 1
         }
 
         L = Integer.parseInt(br.readLine());
@@ -41,16 +41,14 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int move = Integer.parseInt(st.nextToken());
             char c = st.nextToken().charAt(0);
-            if (c == 'L') move = -move;
-            ChangeList.add(move);
+            if (c == 'L') move = -move; // 왼쪽으로 이동하면 move를 음수로
+            ChangeList.add(move); // 오른쪽으로 이동하면 move를 양수로 설정
         }
         ChangeList.add(N);
-
     }
 
     static void pro() throws IOException
     {
-
         while (!ChangeList.isEmpty()) {
             if (!move(ChangeList.poll())) break;
         }
@@ -59,9 +57,9 @@ public class Main {
 
     static boolean move(int destination) {
 
-        int absDest = Math.abs(destination);
+        int absDest = Math.abs(destination); // i값 범위로 사용하기 위해 양수로 변환
 
-        for (int i = MoveCnt; i != absDest; i++) {
+        for (int i = MoveCnt; i != absDest; i++) { // 몇 번째 이동에서 막히는지 판단하라면, 바로 아래에서 MoveCnt++로 확인
             MoveCnt++;
             int ny = curY + dy[dir];
             int nx = curX + dx[dir];
@@ -70,15 +68,14 @@ public class Main {
                 return false;
             }
 
-            if (board[ny][nx] != 1) {
-                int[] lastSnake = SnakeDeque.pollLast();
-                board[lastSnake[0]][lastSnake[1]] = 0;
+            if (board[ny][nx] != 1) { // 사과를 만나지 않은 경우
+                int[] lastSnake = SnakeDeque.pollLast(); // 꼬리 위치 관련 연산
+                board[lastSnake[0]][lastSnake[1]] = 0; // 뒤에 꼬리가 없다는 처리
             }
-            board[ny][nx] = -1;
+            board[ny][nx] = -1; // 사과를 만난 경우 위 처리를 하지 않음, -1은 내 몸이 있는 위치
             SnakeDeque.addFirst(new int[] {ny, nx});
             curY = ny;
             curX = nx;
-
         }
         rotate(destination);
         return true;
@@ -86,6 +83,9 @@ public class Main {
 
     static void rotate(int destination) { // - left, + right
 
+//        static int[] dy = new int[] {0, 1, 0, -1};
+//        static int[] dx = new int[] {1, 0, -1, 0};
+//
         if (destination < 0) { // 왼쪽으로 회전
             dir--;
             if (dir == -1) dir = 3;
