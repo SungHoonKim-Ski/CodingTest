f0456cdb614380922eeff2e565f8c1636556ac4b
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-08-15 22:40:02 +0900

    [Silver I] Title: 신입 사원, Time: 1828 ms, Memory: 303200 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -10,43 +10,38 @@ public class Main
     static StringBuffer sb = new StringBuffer();
     static StringTokenizer st;
     
-    static Person[] Peolpe; 
+    static ArrayList<Person> Peolpe;
     static int N;
     
-    public static class Person implements Comparable<Person> {
+    public static class Person{
         int score1, score2;
         
         Person(int score1, int score2) {
             this.score1 = score1;
             this.score2 = score2;
         }
-        @Override
-        public int compareTo(Person o) {
-            return this.score1 - o.score1;
-        }
     }
     
     public static void input() throws IOException{
         
         N = Integer.parseInt(br.readLine());
-        Peolpe = new Person[N];
+        Peolpe = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            Peolpe[i] = new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            Peolpe.add(new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
     }
 
     public static void pro() {
 
-        Arrays.sort(Peolpe);
-
+        Collections.sort(Peolpe, (o1, o2) -> o1.score1 - o2.score1);
         int res = 1;
-        int min = Peolpe[0].score2;
+        int min = Peolpe.get(0).score2;
 
         for (int i = 1; i < N; i++) {
-            if (Peolpe[i].score2 < min) {
+            if (Peolpe.get(i).score2 < min) {
                 res++;
-                min = Peolpe[i].score2;
+                min = Peolpe.get(i).score2;
             }
         }
         sb.append(res).append('\n');
