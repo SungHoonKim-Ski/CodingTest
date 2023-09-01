714fbbba6ea4d64cbef6977b56b2d57efcb5c75e
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-09-01 17:11:14 +0900

    [Silver II] Title: N과 M (10), Time: 132 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringBuffer setSb = new StringBuffer();
+    static StringTokenizer st;
+
+    static int n, m;
+
+    static int[] inputIntArr;
+
+    static boolean[] visit;
+    static HashSet<String> permutationSet;
+
+    public static void input() throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        inputIntArr = new int[n];
+        visit = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i =0 ; i < n; i++) inputIntArr[i] = Integer.parseInt(st.nextToken());
+        permutationSet = new HashSet<>();
+    }
+
+    public static void pro() {
+
+        Arrays.sort(inputIntArr);
+        backtracking(0, new int[m]);
+        System.out.println(sb);
+
+    }
+
+    public static void backtracking(int depth, int[] elements) {
+
+        if (depth == m) {
+            setSb.setLength(0);
+            for (int i = 0; i < m; i++) setSb.append(elements[i]).append('-');
+            if (permutationSet.add(setSb.toString())) {
+                for (int i = 0; i < m; i++) sb.append(elements[i]).append(' ');
+                sb.append('\n');
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visit[i]) continue;
+            if (depth != 0) {
+                if (elements[depth - 1] > inputIntArr[i]) continue;
+            }
+            visit[i] = true;
+            elements[depth] = inputIntArr[i];
+            backtracking(depth + 1, elements);
+            visit[i] = false;
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+    }
+
+}
