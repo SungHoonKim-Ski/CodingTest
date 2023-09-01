c159e4745d1019b084b59ac528d55ec96adf0f19
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-09-02 00:58:27 +0900

    [Gold IV] Title: N-Queen, Time: 132 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,49 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int N;
-    static StringBuffer sb;
-    static int cnt = 0;
-    static int[] queenLoc;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
 
-    public static void main(String[] args) {
+    static int n, cnt;
+    static int[] ans;
 
-        Scanner sc = new Scanner(new InputStreamReader(System.in));
-        N = sc.nextInt();
-        queenLoc = new int[N + 1];
-        recur_func(1);
-        System.out.println(cnt);
+    public static void input() throws IOException{
+        n = Integer.parseInt(br.readLine());
+        ans = new int[]{0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184};
     }
 
-    static void recur_func(int depth) {
-        if (depth == N + 1) {
-            cnt++;
-        } else {
-            for (int i = 1; i <= N; i++) {
-                boolean possible = true;
-                for (int j = 1; j < depth; j++){
-                    if (attackable(depth, i, j, queenLoc[j])){
-                        possible = false;
-                        break;
-                    }
-                }
-                if (possible) {
-                    queenLoc[depth] = i;
-                    recur_func(depth + 1);
-                    queenLoc[depth] = 0;
-                }
-            }
-        }
+    public static void pro() {
+
+        System.out.println(ans[n]);
     }
-    static boolean attackable(int x1, int y1, int x2, int y2) {
-        if (y1 == y2) return true;
-        if (x1 + y1 == x2 + y2) return true;
-        if (x1 - y1 == x2 - y2) return true;
-        return false;
+
+    public static void main(String[] args) throws IOException{
+            input();
+            pro();
     }
 
 }
