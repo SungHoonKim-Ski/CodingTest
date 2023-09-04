9ee28895386c68652ed5f45632a32a84b8e0991d
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-09-04 23:32:06 +0900

    [Silver I] Title: 보석 상자, Time: 352 ms, Memory: 34800 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -29,18 +29,27 @@ public class Main {
     public static void pro() {
 
         int vaildMid = 0;
-        R *= 2;
-        while (L <= R) {
+        R *= 2; // L의 초기값은 0이고 R의 최대값은 보석들 중 최대값 즉
+        // 보석의 종류만큼만 아이들에게 나누어 줄 수 있게 하는 값이므로
+        // 최대값에 2를 곱해주면 mid가 초기 최대값으로 나옴 
+        while (L < R) {
             int stdCnt = 0;
             int mid = (L + R) / 2;
-            if (mid == L) break;
+            
+            if (mid == L) break; 
+            // 무한 루프 방지
+            // 더이상 나눌 수 없는 경우
+            
             for (int i = 0; i < m; i++) {
                 stdCnt += jewelCnt[i] / mid;
                 if (jewelCnt[i] % mid != 0) stdCnt++;
             }
-            if (stdCnt > n) {
+            if (stdCnt > n) { 
+                // 보석을 mid만큼씩 나누어 줬을 때
+                // 모든 아이들에게 나누어줄 수 없는 경우
                 L = mid;
-            } else { // 가능한 경우
+            } else { 
+                // 가능한 경우
                 vaildMid = mid;
                 R = mid;
             }
