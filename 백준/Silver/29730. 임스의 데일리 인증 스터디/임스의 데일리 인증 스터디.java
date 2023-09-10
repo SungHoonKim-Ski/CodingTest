dd37dd2e6e877f57d9a524723cceca2c26116f12
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-09-11 00:03:29 +0900

    [Silver IV] Title: 임스의 데일리 인증 스터디, Time: 160 ms, Memory: 14944 KB -BaekjoonHub

---
+++ 백준/Silver/29730. 임스의 데일리 인증 스터디/임스의 데일리 인증 스터디.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+//        HashSet<String> set1 = new HashSet<>();
+//        HashSet<String> set2 = new HashSet<>();
+        LinkedList<String> list1 = new LinkedList<>();
+        LinkedList<String> list2 = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            if (s.contains("boj.kr/")) {
+                st = new StringTokenizer(s, "/");
+                st.nextToken();
+                try {
+                    int num = Integer.parseInt(st.nextToken());
+                    if (st.hasMoreTokens()) {
+                        list1.add(s);
+                    } else list2.add(s);
+                } catch (NumberFormatException e) {
+                    list1.add(s);
+                }
+            }
+            else list1.add(s);
+        }
+
+
+        Collections.sort(list1, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) return o1.compareTo(o2);
+                else return o1.length() - o2.length();
+            }
+        });
+        Collections.sort(list2, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) return o1.compareTo(o2);
+                else return o1.length() - o2.length();
+            }
+        });
+
+        for (String s : list1) sb.append(s).append('\n');
+        for (String s : list2) sb.append(s).append('\n');
+        System.out.println(sb);
+    }
+}
