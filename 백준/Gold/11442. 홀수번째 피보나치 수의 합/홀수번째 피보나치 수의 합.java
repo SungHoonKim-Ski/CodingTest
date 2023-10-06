aacce531d1fc6c81227146886245795e5e93e28d
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-10-06 23:58:34 +0900

    [Gold II] Title: 홀수번째 피보나치 수의 합, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Gold/11442. 홀수번째 피보나치 수의 합/홀수번째 피보나치 수의 합.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+    static long n;
+    static final long MOD_VAL = 1_000_000_007;
+    static final long[][] one = {{1, 1}, {1, 0}};
+
+    static void input() throws IOException{
+        n = Long.parseLong(br.readLine());
+    }
+
+    static long[][] mulMatrix(long[][] matrix1, long[][] matrix2) {
+
+        long[][] res = new long[2][2];
+
+        res[0][0] = (matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0]) % MOD_VAL;
+        res[1][0] = (matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0]) % MOD_VAL;
+        res[0][1] = (matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1]) % MOD_VAL;
+        res[1][1] = (matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1]) % MOD_VAL;
+        return res;
+    }
+
+    static long[][] recur(long[][] matrix, long exp) {
+
+        if (exp == 0 || exp == 1) return matrix;
+
+        long[][] div = recur(matrix, exp / 2);
+
+        div = mulMatrix(div, div);
+
+        if (exp % 2 == 1) {
+            div = mulMatrix(div, one);
+        }
+        return div;
+    }
+
+    static void pro() throws IOException{
+        long[][] matrix = recur(one, n - 1);
+        long res = -1;
+        if (n % 2 == 0) res = matrix[1][1] + matrix[0][1];
+        else res = matrix[0][0] + matrix[0][1];
+        res %= MOD_VAL;
+        if (n == 1) res--;
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+    }
+
+}
