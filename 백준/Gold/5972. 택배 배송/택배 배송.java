549afec922ce1ecd9655ce68dd1f29586d6e9087
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-10-23 23:41:08 +0900

    [Gold V] Title: 택배 배송, Time: 440 ms, Memory: 40036 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -48,31 +48,22 @@ public class Main {
     }
 
     static void dijkstra(int start) {
-        // 모든 정점까지에 대한 거리를 무한대로 초기화 해주기.
-        // ※주의사항※
-        // 문제의 정답으로 가능한 거리의 최댓값보다 큰 값임을 보장해야 한다.
-        /* TODO */
+
         for (int i = 1; i <= N; i++) dist[i] = Integer.MAX_VALUE;
 
-        // 최소 힙 생성
-        /* TODO */
         PriorityQueue<Info> pq = new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist);
 
-        // 시작점에 대한 정보(Information)을 기록에 추가하고, 거리 배열(dist)에 갱신해준다.
-        /* TODO */
         dist[start] = 0;
         pq.add(new Info(start, 0));
 
-        // 거리 정보들이 모두 소진될 때까지 거리 갱신을 반복한다.
         while (!pq.isEmpty()) {
             Info info = pq.poll();
 
-            // 꺼낸 정보가 최신 정보랑 다르면, 의미없이 낡은 정보이므로 폐기한다.
             if (info.dist != dist[info.idx]) continue;
-            // 연결된 모든 간선들을 통해서 다른 정점들에 대한 정보를 갱신해준다.
+            
             for (Edge e : edges[info.idx]) {
-                if (dist[e.to] > dist[info.idx] + e.weight) {
-                    dist[e.to] = dist[info.idx] + e.weight;
+                if (dist[e.to] > info.dist + e.weight) {
+                    dist[e.to] = info.dist + e.weight;
                     pq.add(new Info(e.to, dist[e.to]));
                 }
             }
@@ -82,7 +73,6 @@ public class Main {
 
     static void pro() {
         dijkstra(1);
-        System.out.print(sb);
     }
 
     public static void main(String[] args) throws IOException{
