6633c1118c0a591cb3b2408445c2bb3c1e9c7206
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-10-23 23:39:50 +0900

    [Gold III] Title: 파티, Time: 632 ms, Memory: 57760 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -50,18 +50,11 @@ public class Main {
     }
 
     static void dijkstra(int start) {
-        // 모든 정점까지에 대한 거리를 무한대로 초기화 해주기.
-        // ※주의사항※
-        // 문제의 정답으로 가능한 거리의 최댓값보다 큰 값임을 보장해야 한다.
-        /* TODO */
+
         for (int i = 1; i <= N; i++) dist[i] = Integer.MAX_VALUE;
 
-        // 최소 힙 생성
-        /* TODO */
         PriorityQueue<Info> pq = new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist);
 
-        // 시작점에 대한 정보(Information)을 기록에 추가하고, 거리 배열(dist)에 갱신해준다.
-        /* TODO */
         dist[start] = 0;
         pq.add(new Info(start, 0));
 
@@ -71,14 +64,12 @@ public class Main {
 
             // 꺼낸 정보가 최신 정보랑 다르면, 의미없이 낡은 정보이므로 폐기한다.
             if (info.dist != dist[info.idx]) continue;
-            // 연결된 모든 간선들을 통해서 다른 정점들에 대한 정보를 갱신해준다.
+            
             for (Edge e : edges[info.idx]) {
-                if (dist[e.to] > dist[info.idx] + e.weight) {
-                    dist[e.to] = dist[info.idx] + e.weight;
+                if (dist[e.to] > info.dist + e.weight) {
+                    dist[e.to] = info.dist + e.weight;
                     pq.add(new Info(e.to, dist[e.to]));
                 }
-                // e.to 까지 갈 수 있는 더 짧은 거리를 찾았다면 이에 대한 정보를 갱신하고 PQ에 기록해준다.
-                /* TODO */
             }
         }
     }
