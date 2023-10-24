3c6ed75574860d490aa7fdb57f09fa8653e859f4
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-10-24 23:04:17 +0900

    [Gold III] Title: 전단지 돌리기, Time: 440 ms, Memory: 65076 KB -BaekjoonHub

--- 백준/Gold/19542. 전단지 돌리기/전단지 돌리기.java
+++ 백준/Gold/19542. 전단지 돌리기/전단지 돌리기.java
@@ -5,11 +5,15 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static ArrayList<Integer>[] graph;
-    static int N, S, D, distSum;
-
-    static boolean[] visit, isCount;
-
+    static ArrayList<Integer>[] graph; // 입력 그래프
+    
+    static int N, S, D, distSum; // 초기값과 결과값을 위한 distSUm
+    static boolean[] visit, isCount; 
+    // dfs를 위한 visit 배열과
+    // 부모와 자식의 거리가 D 이상인 경우 최소한 해당 부모까지는 방문을 해야만 함
+    // 이 때 distSum++을 함과 동시에 isCount[parent] = true를 통해 해당 부모르
+    // 체크했다는 표시를 위한 배열
+    
     static void input() throws IOException{
          st = new StringTokenizer(br.readLine());
          N = Integer.parseInt(st.nextToken());
@@ -44,18 +48,22 @@ public class Main {
         int maxDist = 0;
 
         for (int nextIdx : graph[curIdx]) {
-            if (visit[nextIdx]) continue;
+            if (visit[nextIdx]) continue; // 양방향 그래프이므로 방문 처리 필요
             visit[nextIdx] = true;
             maxDist = Math.max(maxDist, dfs(nextIdx) + 1);
+            // 부모 기준으로 가장 먼 자식의 거리 + 1이 maxDist
+            // 재귀적으로 자식들을 호출
         }
 
-
         if (!isCount[curIdx] && maxDist >= D) {
+            // 만약 자식과의 거리가 D 이상이면서 이 자식을 세지 않앗다면 카운트++
             isCount[curIdx] = true;
             distSum++;
         }
-
+        
         return maxDist;
+        // 자식 기준으로 가장 먼 자식의 거리를 return
+        // 이 경우 시간 복잡도가 O(N)만에 해결될듯 싶은데..
     }
 
     public static void main(String[] args) throws IOException{
