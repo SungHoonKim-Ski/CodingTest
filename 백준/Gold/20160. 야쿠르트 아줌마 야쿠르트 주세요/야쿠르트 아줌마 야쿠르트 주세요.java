3c17ef79dd76acb2cd107db1dbd2adb87253b19b
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-11-07 22:50:51 +0900

    [Gold III] Title: 야쿠르트 아줌마 야쿠르트 주세요, Time: 800 ms, Memory: 63436 KB -BaekjoonHub

--- 백준/Gold/20160. 야쿠르트 아줌마 야쿠르트 주세요/야쿠르트 아줌마 야쿠르트 주세요.java
+++ 백준/Gold/20160. 야쿠르트 아줌마 야쿠르트 주세요/야쿠르트 아줌마 야쿠르트 주세요.java
@@ -9,6 +9,7 @@ public class Main {
     static StringTokenizer st;
 
     static class Edge{
+        // 간선, 가중치의 정보
         int to, weight;
 
         Edge(int _to, int _weight) {
@@ -17,17 +18,21 @@ public class Main {
     }
 
     static class Info{
+        // 시작점 기준으로 idx 정점까지의 비용을 저장하는 class
+        // PrioirtyQueue에 사용
+
         int idx, dist;
         Info(int _idx, int _dist) {
             idx = _idx; dist = _dist;
         }
     }
+
     static ArrayList<Edge>[] graph;
-//    static HashMap<Integer, Integer>[] graph;
-    static int v, e, start, deliveryStart;
+    static int v, e, start;
+    
     static int[] myMinDist;
-    static HashMap<Integer, int[]> deliveryMinDistMap;
     static int[] deliveryLoc;
+    static HashMap<Integer, int[]> deliveryMinDistMap;
 
     static void input() throws Exception{
         st = new StringTokenizer(br.readLine());
@@ -129,3 +134,34 @@ public class Main {
 
     }
 }
+
+/*  아이디어는 내 정점 기준 다익스트라 + 야구르트 아주머니 기준 다익스트라 10번
+    총 11번 다익스트라를 돌리는데
+
+    전부 동일한 간선을 사용하므로 시작 정점만 다익스트라 함수에 준다면
+    해당 정점 기준으로 모든 정점으로의 최단 거리를 구할 수 있고,
+    이 값을 int[] 에 담아서 return하게 한다면,
+
+    한개의 다익스트라 함수에게 11번 시작 정점을 넣어주고,
+    그 함수 결과로 주는 최단 거리 int[] 배열을 받을 수 있음
+
+    그리고 야구르트 아주머니는 동일한 지점을 재방문 할 수도 있으므로
+    만약 이전에 해당 정점 기준으로 다익스트라를 계산했다면, 또 계산할 필요는 x
+    
+    그래서 내 시작점 기준으로 다익스트라를 돌린 값은 myMinDist[]에
+    야구르트 아주머니 기준 다익스트라를 돌린 값은 HashMap<Integer, int[]> 맵에 저장하고
+    이 때 key는 시작 정점, value는 시작 정점 기준 다른 정점까지의 최단거리
+    
+    
+    이후에 필요한 내용은 아구르트 아주머니가 어떤 정점으로 최단거리로 움직이는데,
+    내가 먼저 또는 동시에 해당 정점에 도착할 수 있는지 여부를 확인해야 함
+    
+    내가 살 수 있는 정점의 번호를 canBuyLoc에,
+    야구르트 아주머니의 현재 위치를 curDeliveryLoc에 저장하고,
+    야구르트 아주머니가 움직일 때마다 움직인 시간을 더해주어야 하므로, 이를 timeSum이라는 변수에 저장
+    
+    이후 아주머니가 다음 정점으로 갈 수 있는지 확인하며
+    다음 정점으로 갈 수 있다면, 야구르트 아주머니의 위치인 curDeliceryLoc 값을 변경해주고
+    해당 정점까지 이동한 총 시간을 timeSum 값에다가 계속 더해줌
+    그리고 만약 야구르트 아주머니의 현재 위치 기준으로 내 최단거리(시간)이 작거나 같다면 이 값을 갱신
+*/
