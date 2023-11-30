88d5d644fb6f5152d88c70dbd4b34fcda64f7064
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-01 00:28:25 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+
+    static void input() throws IOException{
+
+    }
+
+    static void pro() throws IOException{
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+
+    }
+
+}
