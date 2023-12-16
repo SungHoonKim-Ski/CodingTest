5d4bae5aa283b106ad91f0afcc159d4c13671c42
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-17 01:33:53 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 124 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,30 @@
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+
+    static void input() throws IOException{
+
+    }
+
+    static void pro() throws IOException{
+
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+    }
+
+}
