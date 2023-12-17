da6fa0e4542d2e74bf428868725dc867c08f9928
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-17 21:20:15 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 120 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,26 @@
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+
+    static void input() throws IOException{
+
+    }
+
+    static void pro() throws IOException{
+
+        System.out.println("고려대학교");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+    }
+
+}
