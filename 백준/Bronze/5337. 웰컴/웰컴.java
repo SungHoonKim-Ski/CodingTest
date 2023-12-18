ba861d3320c09b5ed32ce6057adac7792625ff46
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-18 21:47:13 +0900

    [Bronze V] Title: 웰컴, Time: 112 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+
+    static void input() throws IOException{
+        
+    }
+
+    static void pro() throws IOException{
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+    }
+
+}
