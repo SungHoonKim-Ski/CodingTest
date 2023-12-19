abe147bef71d2a80bfe5d9f06484ac5d2fbbf1f7
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-19 18:19:16 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 120 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+
+    static void input() throws IOException{
+        
+    }
+
+    static void pro() throws IOException{
+        System.out.println(
+                "    8888888888  888    88888\n" +
+                "   88     88   88 88   88  88\n" +
+                "    8888  88  88   88  88888\n" +
+                "       88 88 888888888 88   88\n" +
+                "88888888  88 88     88 88    888888\n" +
+                "\n" +
+                "88  88  88   888    88888    888888\n" +
+                "88  88  88  88 88   88  88  88\n" +
+                "88 8888 88 88   88  88888    8888\n" +
+                " 888  888 888888888 88  88      88\n" +
+                "  88  88  88     88 88   88888888");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+    }
+
+}
