7aaae7800f027c6f022c508ef08dd98536355973
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-26 00:59:09 +0900

    [Bronze V] Title: NFC West vs North, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+    
+    static void input() throws IOException{
+    }
+
+    static void pro() throws IOException {
+        System.out.println("" +
+                "NFC West       W   L  T\n" +
+                "-----------------------\n" +
+                "Seattle        13  3  0\n" +
+                "San Francisco  12  4  0\n" +
+                "Arizona        10  6  0\n" +
+                "St. Louis      7   9  0\n" +
+                "\n" +
+                "NFC North      W   L  T\n" +
+                "-----------------------\n" +
+                "Green Bay      8   7  1\n" +
+                "Chicago        8   8  0\n" +
+                "Detroit        7   9  0\n" +
+                "Minnesota      5  10  1");
+    }
+
+    public static void main(String[] args) throws IOException{
+            input();
+            pro();
+    }
+
+}
