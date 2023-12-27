3432fa12107e2638d065f7b9d1c8aad9df62752c
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-28 00:33:37 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuffer sb = new StringBuffer();
+    static StringTokenizer st;
+
+    static BigInteger a, b;
+    static void input() throws IOException{
+    }
+
+    static void pro(){
+        System.out.println("1\n0");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+
+    }
+
+}
