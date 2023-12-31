6476c653917734459ba52b32a0fd11b303c2e571
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2023-12-31 18:23:56 +0900

    [Platinum III] Title: 전설, Time: 5804 ms, Memory: 867556 KB -BaekjoonHub

--- 백준/Platinum/19585. 전설/전설.java
+++ 백준/Platinum/19585. 전설/전설.java
@@ -6,7 +6,6 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuffer sb = new StringBuffer();
     static StringTokenizer st;
-    static final int MOD_VAL = 1_000_007;
     static class ColorTrie {
         ColorTrie[] next;
         boolean isLast;
@@ -28,33 +27,12 @@ public class Main {
             trie.isLast = true;
         }
     }
-
-    static class NameTrie {
-        NameTrie[] next;
-        boolean isLast;
-        NameTrie() {
-            next = new NameTrie['z' - 'a' + 1];
-            isLast = false;
-        }
-
-        void addRev(char[] str, int idx) {
-            if (str.length - 1 == idx) {
-                isLast = true;
-            } else {
-                if (next[str[str.length - idx - 1] - 'a'] == null) {
-                    next[str[str.length - idx - 1] - 'a'] = new NameTrie();
-                }
-                next[str[str.length - idx - 1] - 'a'].addRev(str, idx + 1);
-            }
-        }
-    }
     static ColorTrie colorRoot;
-    static NameTrie nameRoot;
     static HashSet<String> nameSet;
 
     static void input() throws IOException{
+        
         colorRoot = new ColorTrie();
-        nameRoot = new NameTrie();
         nameSet = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
@@ -74,20 +52,15 @@ public class Main {
     static void pro() throws IOException{
 
         int q = Integer.parseInt(br.readLine());
-//        HashSet<Integer> colorLengthSet = new HashSet<>();
-//        HashSet<Integer> nameLengthSet = new HashSet<>();
 
         while (q-- > 0) {
 
             ColorTrie colorTrie = colorRoot;
-//            NameTrie nameTrie = nameRoot;
             boolean isCorrect = false;
 
             String teamName = br.readLine();
             char[] teamNameArr = teamName.toCharArray();
             int teamNameLength = teamNameArr.length;;
-
-//            int index = 0;
 
             for (int i = 0; i < teamNameLength; i++) {
                 colorTrie = colorTrie.next[teamNameArr[i] - 'a'];
@@ -101,38 +74,6 @@ public class Main {
                     }
                 }
             }
-
-//
-//            while (colorTrie != null) {
-//
-//                if (index == teamNameLength) break;
-//
-//                if (colorTrie.isLast) {
-//                    colorLengthSet.add(index + 1);
-//                    if (nameLengthSet.contains(teamNameLength - index + 1)) {
-//                        isCorrect = true;
-//                        break;
-//                    }
-//                }
-//
-//                colorTrie = colorTrie.next[teamNameArr[index] - 'a'];
-//                index++;
-//            }
-
-//
-//            index = 0;
-//            while (nameTrie != null) {
-//                if (index == teamNameArr.length) break;
-//                if (nameTrie.isLast) {
-//                    nameLengthSet.add(index + 1);
-//                    if (colorLengthSet.contains(teamNameLength - index - 1)) {
-//                        isCorrect = true;
-//                        break;
-//                    }
-//                }
-//                nameTrie = nameTrie.next[teamNameArr[teamNameLength - index - 1] - 'a'];
-//                index++;
-//            }
 
             if (!isCorrect) sb.append("No");
             else sb.append("Yes");
