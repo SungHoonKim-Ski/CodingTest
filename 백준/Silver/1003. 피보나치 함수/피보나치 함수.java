1349bba58f8940b67a05371822ba8a8ba61a5aed
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2024-01-14 14:47:12 +0900

    [Silver III] Title: 피보나치 함수, Time: 132 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,51 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-
 public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static int[][] dp;
+    static int n;
 
-    //    private static Scanner sc = new Scanner(System.in);
-
-    static int[] zeroCountArr;
-    static int[] oneCountArr;
-    static int[] fibonacciArr;
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-
-        int count = Integer.parseInt(br.readLine());
-
-        zeroCountArr = new int[41];
-        oneCountArr = new int[41];
-        fibonacciArr = new int[41];
-
-        for(int i=0; i<=40; i++){
-            Fibonacci(i);
+    static void init() {
+        dp = new int[40 + 1][2];
+        dp[0][0] = 1;
+        dp[1][1] = 1;
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
         }
+    }
+    static void input() {
+        n = Integer.parseInt(scan.nextLine());
+    }
 
-        for(int i=0; i<count; i++){
-            int input = Integer.parseInt(br.readLine());
-            sb.append(zeroCountArr[input] + " " + oneCountArr[input] + "\n");
+    static void pro() {
+        sb.append(dp[n][0]).append(' ').append(dp[n][1]).append('\n');
+    }
+
+    public static void main(String[] args) {
+
+        init();
+        int tc = Integer.parseInt(scan.nextLine());
+        while (tc -- > 0) {
+            input();
+            pro();
         }
-
-        sb.setLength(sb.length()-1);
         System.out.println(sb);
     }
-    static void Fibonacci(int n) {
-        if (n == 0) {
-            zeroCountArr[0] = 1;
-            fibonacciArr[0] = 0;
-        } else if (n == 1) {
-            oneCountArr[1] = 1;
-            fibonacciArr[1] = 1;
-        } else {
-            fibonacciArr[n] =  fibonacciArr[n-1] + fibonacciArr[n-2];
-            oneCountArr[n] =  oneCountArr[n-1] + oneCountArr[n-2];
-            zeroCountArr[n] =  zeroCountArr[n-1] + zeroCountArr[n-2];
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
-
 }
