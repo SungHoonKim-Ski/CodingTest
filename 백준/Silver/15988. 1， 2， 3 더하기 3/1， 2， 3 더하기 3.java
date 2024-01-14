52720cc276a5d98c0b2f08f3fe2e50a597780e97
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2024-01-14 15:07:10 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 152 ms, Memory: 22084 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -2,43 +2,83 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuffer sb = new StringBuffer();
-    static StringTokenizer st;
-
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static long[] dp;
     static int n;
 
-    static long[] dp;
-    static void input() throws IOException{
-        n = Integer.parseInt(br.readLine());
-        dp = new long[n + 1];
+    static void init() {
+        dp = new long[1_000_000 + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009;
+        }
+    }
+    static void input() {
+        n = scan.nextInt();
     }
 
     static void pro() {
-
-        if (n < 3) {
-            if (n == 1) sb.append(1).append('\n');
-            else if (n == 2) sb.append(2).append('\n');
-        } else {
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-
-            for (int i = 4; i <= n; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
-            }
-            sb.append(dp[n]).append('\n');
-        }
+        sb.append(dp[n]).append('\n');
     }
 
-    public static void main(String[] args) throws IOException{
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
+    public static void main(String[] args) {
+
+        init();
+        int tc = Integer.parseInt(scan.nextLine());
+        while (tc -- > 0) {
             input();
             pro();
         }
         System.out.println(sb);
     }
 
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
