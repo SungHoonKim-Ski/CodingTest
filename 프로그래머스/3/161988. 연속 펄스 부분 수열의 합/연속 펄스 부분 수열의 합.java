e384cddc75c665563bd0ccca31d53fcbe605fdc8
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2024-03-12 23:21:15 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 33.78 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -8,9 +8,6 @@ class Solution {
     
     public long solution(int[] sequence) {
         n = sequence.length;
-        
-        if (n == 1) return Math.max(sequence[0], -sequence[0]);
-        
         aDp = new long[n];
         bDp = new long[n];
         aSeq = new long[n];
@@ -31,19 +28,24 @@ class Solution {
         aDp[0] = aSeq[0];
         bDp[0] = bSeq[0];
         
-        aDp[1] = Math.max(aDp[0], 0) + aSeq[1];
-        bDp[1] = Math.max(bDp[0], 0) + bSeq[1];
-
-        long answer = Integer.MIN_VALUE;
-
+        aDp[1] = Math.max(aDp[0], Math.max(aDp[0] + aSeq[1], aSeq[1]));
+        bDp[1] = Math.max(bDp[0], Math.max(bDp[0] + bSeq[1], bSeq[1]));
+        
         for (int i = 2; i < n; i++) {
-            aDp[i] = Math.max(aDp[i - 1], 0) + aSeq[i];
-            answer = Math.max(aDp[i], answer);
-            bDp[i] = Math.max(bDp[i - 1], 0) + bSeq[i];
-            answer = Math.max(bDp[i], answer);
+            
+            if (aDp[i - 2] + aSeq[i - 1] != aDp[i - 1] && aDp[i - 1] != aSeq[i - 1]) { // 연속이 아니라면
+                aDp[i] = Math.max(aDp[i - 1], aSeq[i]);
+            } else {
+                aDp[i] = Math.max(aDp[i - 1], Math.max(aDp[i - 1] + aSeq[i], aSeq[i]));
+            }
+            
+            if (bDp[i - 2] + bSeq[i - 1] != bDp[i - 1] && bDp[i - 1] != bSeq[i - 1]) { // 연속이 아니라면
+                bDp[i] = Math.max(bDp[i - 1], bSeq[i]);
+            } else {
+                bDp[i] = Math.max(bDp[i - 1], Math.max(bDp[i - 1] + bSeq[i], bSeq[i]));
+            }
         }
-//        System.out.println(Arrays.toString(aDp));
-//        System.out.println(Arrays.toString(bDp));
+        long answer = Math.max(aDp[n - 1], bDp[n - 1]);
         return answer;
     }
     
