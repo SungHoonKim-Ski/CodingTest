9e4d1a682bdb6d6fd052e5c0fa963b0c71a17451
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2024-06-11 21:56:30 +0900

    [Gold IV] Title: N으로 만들기, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/17255. N으로 만들기/N으로 만들기.java
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.java
@@ -10,56 +10,34 @@ class Main {
     static int MAX_VALUE = 10_000_000;
     static int nNum, len, candidateCnt;
     static String n;
-    static HashSet<String> validNumSet;
+    static HashSet<String> validNumSet, visitSet;
     static HashSet<Integer> nSet;
     static void input() throws IOException{
         n = br.readLine();
-        nNum = Integer.parseInt(n);
-        validNumSet = new HashSet<>();
+        sb = new StringBuilder(n);
     }
 
     static void pro() {
 
-        char[] nToCharArr = n.toCharArray();
-        len = nToCharArr.length;
-        nSet = new HashSet<>();
-
-        for (int i = 0; i < len; i++) {
-            nSet.add(nToCharArr[i] - '0');
-        }
-
-        for (int i : nSet) {
-            int[] visit = new int[len];
-            visit[0] = i + MAX_VALUE;
-            dfs(1, visit, String.valueOf(i));
-        }
-
+        dfs(sb);
 
         System.out.println(candidateCnt);
     }
 
-    static void dfs(int depth, int[] visit, String curNum) {
+    static void dfs(StringBuilder sb) {
 
-        if (depth == len) {
-            if (curNum.equals(n)) {
-                if (validNumSet.add(Arrays.toString(visit))) candidateCnt++;
-            }
+        if (sb.length() == 0) {
+            candidateCnt++;
             return;
         }
 
-        for (int i: nSet) {
-            String num1 = curNum + i;
-            int[] visit1 = visit.clone();
-            visit1[depth] = i + MAX_VALUE;
-
-            dfs(depth + 1, visit1 ,num1);
-
-            String num2 = i + curNum;
-            if (num1.equals(num2)) continue;
-            int[] visit2 = visit.clone();
-            visit2[depth] = -(i + MAX_VALUE);
-            dfs(depth + 1, visit2, num2);
-        }
+        StringBuilder sb1 = new StringBuilder(sb);
+        sb1.deleteCharAt(0);
+        StringBuilder sb2 = new StringBuilder(sb);
+        sb2.deleteCharAt(sb2.length() - 1);
+        dfs(sb1);
+        if (sb1.toString().contentEquals(sb2)) return;
+        dfs(sb2);
     }
 
     public static void main(String[] args) throws Exception{
