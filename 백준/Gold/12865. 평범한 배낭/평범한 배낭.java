b1de81fd12efe45069f0715685f9c803273fcce1
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-05-27 22:36:39 +0900

    [Gold V] Title: 평범한 배낭, Time: 148 ms, Memory: 50864 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 
 public class Main {
 
-    //    private static Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-    public static void main(String args[]) throws IOException {
+    static int n, k;
+    static int[][] dp;
+    static int[][] input;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] inputSplit =  br.readLine().split(" ");
-        int itemsCount = Integer.parseInt(inputSplit[0]);
-        int maxWeight = Integer.parseInt(inputSplit[1]);
-
-        int[][] backPack = new int[itemsCount+1][maxWeight+1];
-        int[] weights = new int[itemsCount+1];
-        int[] values = new int[itemsCount+1];
-
-        for(int i=1;i<=itemsCount;i++) {
-            inputSplit =  br.readLine().split(" ");
-            weights[i] = Integer.parseInt(inputSplit[0]);
-            values[i]= Integer.parseInt(inputSplit[1]);
+    static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        
+        input = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            input[i][0] = Integer.parseInt(st.nextToken()); // weight
+            input[i][1] = Integer.parseInt(st.nextToken()); // value
         }
-
-        for(int i=1;i<=itemsCount;i++) {
-            for(int j=1;j<=maxWeight;j++) {
-                backPack[i][j] = backPack[i-1][j];
-                if(j - weights[i]>=0) {
-                    backPack[i][j] = Math.max(backPack[i-1][j], backPack[i-1][j-weights[i]]+values[i]);
-                }
-            }
-        }
-
-        System.out.println(backPack[itemsCount][maxWeight]);
     }
 
+    public static void pro() {
+        int[][] dp = new int[n][k + 1];
+        for (int i = 1; i <= k; i++) {
+            if (i < input[0][0]) continue;
+            dp[0][i] = input[0][1];
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j < input[i][0]) continue;
+
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - input[i][0]] + input[i][1]);
+            }
+        }
+        
+        System.out.println(dp[n - 1][k]);
+    }
+    
+    public static void main(String[] args) throws Exception{
+        input();
+        pro();
+    }
 }
+
+
