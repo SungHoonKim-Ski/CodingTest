7b0fc048a302196271dd9b894a550561b98a121b
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-05-28 20:10:00 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,84 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
-    static int[] dp;
-    static int n;
 
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int n;
+    static int[] dp;
+    
     static void init() {
         dp = new int[12];
+        dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        dp[4] = 7;
-        for (int i = 4; i < 12; i++) {
+
+        for (int i = 4; i <= 11; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
     }
-    static void input() {
-        n = Integer.parseInt(scan.nextLine());
+
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
     }
 
     static void pro() {
-       sb.append(dp[n]).append('\n');
+        sb.append(dp[n]).append('\n');
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
         init();
-        int tc = scan.nextInt();
-        while (tc-- > 0) {
+        int tc = Integer.parseInt(br.readLine());
+        while (tc -- > 0) {
             input();
             pro();
         }
         System.out.println(sb);
-    }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
     }
 }
