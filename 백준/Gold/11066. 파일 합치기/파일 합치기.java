bb9a734d9a3db85fd6722b0aee053fc4b3780556
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-06-03 00:22:39 +0900

    [Gold III] Title: 파일 합치기, Time: 776 ms, Memory: 30448 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,53 +1,58 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
-    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
     static int n;
-    static int[] nums;
-    static int[][] prefixSum, dp;
+    static int[] files;
+    static int[][] prefixSum;
+    static int[][] dp;
 
-    static void input() throws IOException {
+
+    static void input() throws Exception {
         n = Integer.parseInt(br.readLine());
-
-        nums = new int[n + 1];
+        files = new int[n + 1];
         st = new StringTokenizer(br.readLine());
-
         for (int i = 1; i <= n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            files[i] = Integer.parseInt(st.nextToken());
         }
 
         prefixSum = new int[n + 1][n + 1];
+        prefixSum[1][1] = files[1];
         for (int i = 1; i <= n; i++) {
             for (int j = i; j <= n; j++) {
-                prefixSum[i][j] = prefixSum[i][j - 1] + nums[j];
+                prefixSum[i][j] = prefixSum[i][j - 1] + files[j];
             }
         }
-        dp = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) Arrays.fill(dp[i], -1);
     }
 
-    static void pro() {
+    public static void pro() {
+        dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+            dp[i][i] = 0;
+        }
+
         System.out.println(recur(1, n));
     }
 
     static int recur(int i, int j) {
-        if (i == j) return 0;
         if (dp[i][j] != -1) return dp[i][j];
 
         dp[i][j] = Integer.MAX_VALUE;
         for (int x = i; x < j; x++) {
-            dp[i][j] = Math.min(recur(i, x) + recur(x + 1, j) + prefixSum[i][j], dp[i][j]);
+            dp[i][j] = Math.min(dp[i][j], prefixSum[i][j] + recur(i, x) + recur(x + 1, j));
         }
 
         return dp[i][j];
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             input();
