0e92330d1497f6c733cab7df64c61031694fb5c8
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-06-05 00:47:11 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1456 ms, Memory: 95660 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,115 +1,65 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 
-class A {
-    int a;
-    A(int a) {
-        this.a = a;
-    }
-}
-
-class B{
-    int b;
-    B(int b) {
-        this.b = b;
-    }
-}
-
-
 public class Main {
-    static FastReader scan = new FastReader();
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-    static int n, R, Q;
-    static ArrayList<Integer>[] tree;
+    static int n, r, q;
+    static ArrayList<Integer>[] graph;
+    static int[] dp;
+    static boolean[] visit;
 
-    static int[] Dy;
+    static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
 
-    static void input() throws IOException {
-        n = scan.nextInt();
-        R = scan.nextInt();
-        Q = scan.nextInt();
-        tree = new ArrayList[n + 1];
-        Dy = new int[n + 1];
-        for (int i = 1; i <= n; i++) tree[i] = new ArrayList<>();
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
         for (int i = 1; i < n; i++) {
-            int v1 = scan.nextInt();
-            int v2 = scan.nextInt();
-            tree[v1].add(v2);
-            tree[v2].add(v1);
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
         }
     }
 
-    static void dfs(int x, int prev) {
+    public static void pro() throws IOException {
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
 
-        Dy[x] = 1;
-        for (int t: tree[x]) {
-            if (t == prev) continue;
-            dfs(t, x);
-            Dy[x] += Dy[t];
+        visit = new boolean[n + 1];
+        recur(r);
+
+        while (q-- > 0) {
+            System.out.println(dp[Integer.parseInt(br.readLine())]);
         }
     }
 
-    static void pro() {
+    static int recur(int cur) {
+        visit[cur] = true;
+        if (dp[cur] != -1) return -1;
 
-        dfs(R, -1);
+        dp[cur] = 1;
 
-        while (Q-- > 0) sb.append(Dy[scan.nextInt()]).append('\n');
-        System.out.println(sb);
+        for (int next: graph[cur]) {
+            if (visit[next]) continue;
+            dp[cur] += recur(next);
+        }
+
+        return dp[cur];
     }
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         input();
         pro();
-    }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
     }
 }
