502a5c4b753bb9b01496e3f031919fd8329f9a57
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-06-09 18:01:47 +0900

    [Silver III] Title: 블로그, Time: 252 ms, Memory: 38592 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -9,38 +9,41 @@ public class Main {
     static StringTokenizer st;
 
     static int n, x;
-    static long[] prefixSum, visitNum;
+    static int[] visitNum;
 
     static void input() throws Exception {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
 
-        visitNum = new long[n];
-        prefixSum = new long[n];
+        visitNum = new int[n];
         st = new StringTokenizer(br.readLine());
 
-        prefixSum[0] = visitNum[0] = Integer.parseInt(st.nextToken());
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             visitNum[i] = Integer.parseInt(st.nextToken());
-            prefixSum[i] = prefixSum[i - 1] + visitNum[i];
         }
     }
 
     public static void pro() {
-        long max = prefixSum[x - 1], count = 1;
+        int sum = 0;
+        for (int i = 0; i < x; i++) {
+            sum += visitNum[i];
+        }
+        int max = sum;
+        int count = sum != 0 ? 1 : 0;
         
-        for (int i = x; i < n; i++) {
-            long sum = prefixSum[i] - prefixSum[i - x];
+        int right = x;
+        for (int left = 0; left < n - x; left++) {
+            sum -= visitNum[left];
+            sum += visitNum[right++];
+
             if (sum > max) {
                 max = sum;
                 count = 1;
                 continue;
             }
 
-            if (sum == max) {
-                count++;
-            }
+            if (sum == max) count++;
         }
 
         if (max == 0) System.out.println("SAD");
