caafcca913baca17139633c982d3294123171221
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-06-09 14:45:50 +0900

    [Gold I] Title: 계단 수, Time: 84 ms, Memory: 16172 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,58 +1,58 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static StringBuffer sb = new StringBuffer();
 
     static int n;
+    static int TEN_BIT = 1 << 10, MAX = 100, MOD = 1_000_000_000;
     static int[][][] dp;
-    static final int MOD = 1_000_000_000;
-    static int ZERO_TO_NINE = (1 << 10);
 
-    static void input() throws IOException {
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n + 1][10][ZERO_TO_NINE];
-        for (int[][] arr2d : dp) {
-            for (int[] arr1d : arr2d) {
-                Arrays.fill(arr1d, -1);
-            }
-        }
-    }
+    static int recur(int depth, int curNum, int visit) {
+        if (dp[depth][curNum][visit] != -1) return dp[depth][curNum][visit];
 
-    static int dfs(int length, int lastDigit, int visited) {
-        if (length == n) {
-            return visited == (ZERO_TO_NINE - 1) ? 1 : 0;
-        }
-        if (dp[length][lastDigit][visited] != -1) {
-            return dp[length][lastDigit][visited];
+        if (depth == 1) {
+            return visit == (TEN_BIT - 1) ? 1 : 0;
         }
 
-        int result = 0;
-        if (lastDigit > 0) {
-            result += dfs(length + 1, lastDigit - 1, visited | (1 << (lastDigit - 1)));
-            result %= MOD;
-        }
-        if (lastDigit < 9) {
-            result += dfs(length + 1, lastDigit + 1, visited | (1 << (lastDigit + 1)));
-            result %= MOD;
-        }
-        dp[length][lastDigit][visited] = result;
-        return result;
-    }
-
-    static void pro() {
         int sum = 0;
-        for (int i = 1; i < 10; i++) {
-            sum += dfs(1, i, 1 << i);
+        if (curNum != 0) {
+            sum += recur(depth - 1, curNum - 1, visit | (1 << (curNum - 1)));
             sum %= MOD;
         }
+
+        if (curNum != 9) {
+            sum += recur(depth - 1, curNum + 1, visit | (1 << (curNum + 1)));
+            sum %= MOD;
+        }
+
+        return dp[depth][curNum][visit] = sum;
+    }
+
+    static void input() throws Exception {
+        n = Integer.parseInt(br.readLine());
+    }
+
+    public static void pro() {
+        dp = new int[n + 1][10][TEN_BIT];
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j < 10; j++)
+                Arrays.fill(dp[i][j], -1);
+
+        int sum = 0;
+        for (int i = 1; i < 10; i++) {
+            sum += recur(n, i, 1 << i);
+            sum %= MOD;
+        }
+
         System.out.println(sum);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         input();
         pro();
     }
