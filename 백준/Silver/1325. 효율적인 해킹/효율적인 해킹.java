89367f3af292e274462ab2faa8e89fa151ae35a9
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-06-10 18:33:57 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10656 ms, Memory: 303464 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,86 +1,81 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuffer sb = new StringBuffer();
+    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int n, m;
 
+    static int n, m;
     static ArrayList<Integer>[] graph;
 
-    static boolean[] visit;
-
-    static int[] hackCnt;
-    public static void input() throws IOException{
-
-
+    static void input() throws Exception {
         st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-
         graph = new ArrayList[n + 1];
-        hackCnt = new int[n + 1];
-        visit = new boolean[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            graph[s].add(e);
-        }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
+            graph[b].add(a);
+        }
     }
 
     public static void pro() {
-
-
+        int max = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            bfs(i);
+            int hackCnt = bfs(i);
+            if (max < hackCnt) {
+                max = hackCnt;
+                nums.clear();
+                nums.add(i);
+                continue;
+            }
 
+            if (max == hackCnt) nums.add(i);
         }
 
-        int maxCount = -1;
-        for (int i = 1; i <= n; i++) {
-            if (maxCount < hackCnt[i]) {
-                sb.setLength(0);
-                sb.append(i).append(' ');
-                maxCount = hackCnt[i];
-            } else if (maxCount == hackCnt[i]) sb.append(i).append(' ');
+        Collections.sort(nums);
+        for (int num: nums) {
+            sb.append(num).append(' ');
         }
         System.out.println(sb);
     }
 
-    public static void bfs(int start) {
+    private static int bfs(int start) {
+        boolean[] visit = new boolean[n + 1];
 
-        Deque<Integer> queue = new ArrayDeque<>();
-        visit = new boolean[n + 1];
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(start);
         visit[start] = true;
-        queue.add(start);
+        int count = -1;
 
-        while (!queue.isEmpty()) {
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+            count++;
 
-            int cur = queue.poll();
-
-            for (int next : graph[cur]) {
+            for (int next: graph[cur]) {
                 if (visit[next]) continue;
                 visit[next] = true;
-                hackCnt[next]++;
-                queue.add(next);
+                que.add(next);
             }
         }
+
+        return count;
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception{
         input();
         pro();
     }
-
 }
