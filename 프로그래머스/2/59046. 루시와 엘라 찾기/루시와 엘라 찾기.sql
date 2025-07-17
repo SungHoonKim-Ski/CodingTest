846211a764da03b45d1c5f82398a4e48333cf022
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-07-17 22:08:07 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+SELECT
+ANIMAL_ID
+, NAME
+, SEX_UPON_INTAKE
+FROM
+ANIMAL_INS
+WHERE
+NAME = 'Lucy'
+OR
+NAME = 'Ella'
+OR
+NAME = 'Pickle'
+OR
+NAME = 'Sabrina'
+OR
+NAME = 'Mitty'
+ORDER BY
+ANIMAL_ID ASC
