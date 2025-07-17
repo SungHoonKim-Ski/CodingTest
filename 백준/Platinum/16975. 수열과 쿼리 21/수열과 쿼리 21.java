206d79e318656f6a6c049e38d1222c959f47cda4
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-07-18 02:16:03 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 632 ms, Memory: 91804 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static final int LEN = 1 << 20;
+    static final int SIZE = 1 << 21;
+
+    static int n, q;
+    static long[] tree, lazy, arr;
+
+    static void input() throws Exception {
+        n = Integer.parseInt(br.readLine());
+
+        arr = new long[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    public static void pro() throws IOException {
+        makeTree();
+
+        q = Integer.parseInt(br.readLine());
+        while (q-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+
+            if (cmd == 1) {
+                int i = Integer.parseInt(st.nextToken()) - 1;
+                int j = Integer.parseInt(st.nextToken()) - 1;
+                long k = Long.parseLong(st.nextToken());
+                update(1, 0, LEN - 1, i, j, k);
+            } else {
+                int a = Integer.parseInt(st.nextToken()) - 1;
+                sb.append(get(1, 0, LEN - 1, a, a)).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static void updateLazy(int idx, int s, int e) {
+        if (lazy[idx] != 0) {
+            tree[idx] += (e - s + 1) * lazy[idx];
+            if (s != e) {
+                lazy[idx * 2] += lazy[idx];
+                lazy[idx * 2 + 1] += lazy[idx];
+            }
+            lazy[idx] = 0;
+        }
+    }
+
+    static void update(int idx, int s, int e, int ts, int te, long add) {
+        updateLazy(idx, s, e);
+
+        if (s > te || e < ts) return;
+        if (ts <= s && e <= te) {
+            tree[idx] += (e - s + 1) * add;
+            if (s != e) {
+                lazy[idx * 2] += add;
+                lazy[idx * 2 + 1] += add;
+            }
+            lazy[idx] = 0;
+            return;
+        }
+
+        int mid = (s + e) / 2;
+        update(idx * 2, s, mid, ts, te, add);
+        update(idx * 2 + 1, mid + 1, e, ts, te, add);
+        tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+    }
+
+    static long get(int idx, int s, int e, int ts, int te) {
+        updateLazy(idx, s, e);
+
+        if (s > te || e < ts) return 0;
+        if (ts <= s && e <= te) return tree[idx];
+
+        int mid = (s + e) / 2;
+        return get(idx * 2, s, mid, ts, te)
+                + get(idx * 2 + 1, mid + 1, e, ts, te);
+    }
+
+
+    static void makeTree() {
+        tree = new long[SIZE];
+        lazy = new long[SIZE];
+
+        for (int i = 0; i < n; i++) {
+            tree[i + LEN] = arr[i];
+        }
+
+        for (int i = LEN - 1; i > 0; i--) {
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        input();
+        pro();
+    }
+}
