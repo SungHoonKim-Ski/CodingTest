2884500c4c8e6ef48bf71ed49f8c6f0853b55438
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2025-10-30 20:53:14 +0900

    [Silver I] Title: 댄스타임, Time: 836 ms, Memory: 53080 KB -BaekjoonHub

---
+++ 백준/Silver/30625. 댄스타임/댄스타임.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static int[][] arr;
+    static long[][] dp;
+
+    static long MOD = 1_000_000_007;
+
+    static void input() throws IOException{
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        }
+    }
+
+    static void pro() {
+        dp = new long[n][2];
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        System.out.println(recur(0, 0));
+    }
+
+    static long recur(int depth, int wrong) {
+        if (wrong > 1) return 0;
+        if (depth == n) return 1;
+
+        if (dp[depth][wrong] != -1) return dp[depth][wrong];
+
+        long sum = 0;
+
+        if (arr[depth][1] == 0) {
+            for (int i = 1; i <= m; i++) {
+                if (arr[depth][0] == i) continue;
+                sum += recur(depth + 1, wrong + 1);
+                sum %= MOD;
+            }
+            sum += recur(depth + 1, wrong);
+            sum %= MOD;
+        } else {
+            sum += recur(depth + 1, wrong + 1);
+            sum %= MOD;
+
+            for (int i = 1; i <= m; i++) {
+                if (arr[depth][0] == i) continue;
+                sum += recur(depth + 1, wrong);
+                sum %= MOD;
+            }
+        }
+
+        return dp[depth][wrong] = sum % MOD;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+}
